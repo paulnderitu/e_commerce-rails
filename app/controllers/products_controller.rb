9de826c9b8110ef2744b1dd574c928a2fc6ea747
Your Name <youremail@domain.com>
2017-08-19 21:30:49 +0300

    added a controller action for updating a product. Next is destroy

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,6 +8,7 @@ class ProductsController < ApplicationController
     @brand = Brand.find(params[:brand_id])
     @product = @brand.products.new(product_params)
     if @product.save
+      flash[:notice] = 'Product added successfully!'
       redirect_to brand_path(@product.brand)
     else
       render :new
@@ -18,6 +19,17 @@ class ProductsController < ApplicationController
     @brand = Brand.find(params[:brand_id])
     @product = @brand.products.find(params[:id])
     render :edit
+  end
+
+  def update
+    @brand = Brand.find(params[:brand_id])
+    @product = @brand.products.find(params[:id])
+    if @product.update(product_params)
+      flash[:notice] = 'Product updated successfully!'
+      redirect_to brands_path
+    else
+      render :edit
+    end
   end
 
   private
