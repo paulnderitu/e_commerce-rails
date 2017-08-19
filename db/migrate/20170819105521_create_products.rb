583badbf2d7fa21b5a5a7a356abf7f90f3b5dfbb
Your Name <youremail@domain.com>
2017-08-19 14:00:31 +0300

    migrated products params to database

--- db/migrate/20170819105521_create_products.rb
+++ db/migrate/20170819105521_create_products.rb
@@ -1,6 +1,12 @@
 class CreateProducts < ActiveRecord::Migration[5.1]
   def change
     create_table :products do |t|
+      t.column :item, :string
+      t.column :vote, :integer
+      t.column :brand_id, :integer
+      t.column :user_id, :integer
+
+      t.timestamps
     end
   end
 end
