c9345b2a98151e5874ed773915805b76a624b87d
Your Name <youremail@domain.com>
2017-08-19 13:21:28 +0300

    added a controller action for creating a new brand, received it on new.html.erb and also added a link to the index to allow me visit the page for adding a new brand

--- app/controllers/brands_controller.rb
+++ app/controllers/brands_controller.rb
@@ -10,4 +10,19 @@ class BrandsController < ApplicationController
   def new
     @brand = Brand.new
   end
+
+  def create
+    @brand = Brand.new(brand_params)
+    if @brand.save
+      redirect_to brands_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def brand_params
+    params.require(:brand).permit(:title, :link, :picture)
+  end
 end
