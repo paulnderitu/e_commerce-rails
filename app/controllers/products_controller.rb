2c66316e720373e5e08f15bde45fbd46cecdb7bb
Your Name <youremail@domain.com>
2017-08-19 21:09:22 +0300

    added a controller action for creating a new product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,4 +3,20 @@ class ProductsController < ApplicationController
     @brand = Brand.find(params[:brand_id])
     @product = @brand.products.new
   end
+
+  def create
+    @brand = Brand.find(params[:brand_id])
+    @product = @brand.products.new(product_params)
+    if @product.save
+      redirect_to brand_path(@product.brand)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:item)
+  end
 end
