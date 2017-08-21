73d9b6ab77e0c894ee64cf163c24faf17c7e22bf
Your Name <youremail@domain.com>
2017-08-21 13:31:11 +0300

    added code for authenticating an admin

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,6 +1,19 @@
 class AdminsController < ApplicationController
+  before_action :authenticate_user!
   def index
     @users = User.all
     render :index
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def destroy
+    @brand = Brand.find(params[:brand_id])
+    @product = @brand.products.find(params[:id])
+    @product.destroy
+    flash[:notice] = 'Product deleted successfully!'
+    redirect_to brands_path
   end
 end
