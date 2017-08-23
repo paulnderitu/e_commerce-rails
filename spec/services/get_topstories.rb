b3e405016b005397e22b839e89f3a529f5f3bc29
Your Name <youremail@domain.com>
2017-08-23 12:10:17 +0300

    added another spec to test if the right content was returned

--- spec/services/get_topstories.rb
+++ spec/services/get_topstories.rb
@@ -5,4 +5,9 @@ describe Topstories do
     response = Topstories.get_topstories
     expect(response.code).to(eq(200))
   end
+
+  it 'returns stories when the API call is made' do
+    response = JSON.parse(Topstories.get_topstories)
+    expect(response['results']['stories']).to_not(eq(nil))
+  end
 end
