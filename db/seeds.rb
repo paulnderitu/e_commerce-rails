184b45647344687ed76a15f4de461d92379d0899
Your Name <youremail@domain.com>
2017-08-25 15:09:30 +0300

    practiced seeding my brands table, although did not migrate it to database cz already existed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+brand_detail = [
+  ['Dr.Dre', 'www.google.com', '']
+]
+brand_detail.each do |title, link|
+  Brand.create(title: title, link: link)
+end
