c09feb80e4a0aff7bedcbf2b652c7383a269b2e0
Your Name <youremail@domain.com>
2017-08-31 11:19:22 +0300

    added controller action for creating a new category/field

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -8,6 +8,21 @@ class CategoriesController < ApplicationController
   end
 
   def new
-    @list = Categories.new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      redirect_to categories_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
   end
 end
